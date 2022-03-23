c701d1cbdcbe43b2fbeabea6d18bbef57b8701ea
zhdanushka <gopotron@list.ru>
2022-03-23 12:12:39 +0500

    Задание по скриптам 2

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,38 @@
 export function romanToInteger(str) {
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    let array = str.split('');
+    let curr = 0;
+    let pred = 0;
+    let predRes = 0;
+    for (let i = 0; i < str.length; i++) {
+        curr = char(array[i]);
+        if (curr > pred) result -= 2 * predRes;
+        if (curr !== pred) predRes = 0;
+        predRes += curr;
+        result += curr;
+        pred = curr;
+    }
+
+    function char(char) {
+        switch (char) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return -1;
+        }
+    }
+
     return result;
 }
